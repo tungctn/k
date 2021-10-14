de369af52471bb30505ce2d19211adeae639132f
tungctn <78474449+tungctn@users.noreply.github.com>
2021-10-14 12:09:53 +0700

    Add files via upload

--- test.java
+++ test.java
@@ -11,12 +11,27 @@ public class test {
         DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
         //DictionaryManagement dictionaryManagement = new DictionaryManagement();
         // test với số lượng từ là 6, copy các từ trong in.txt
+        Scanner sc = new Scanner(System.in);
         dictionaryCommandline.dictionaryManagement.insertFromCommandline();
-        //dictionaryCommandline.dictionaryManagement.repair();
-        //dictionaryManagement.delete("lik");
-        //dictionaryCommandline.dictionaryManagement.lookup();
-        dictionaryCommandline.Searcher();
-        dictionaryCommandline.dictionaryManagement.showAllWord();
+
+        while (true) {
+
+            System.out.println("Chon chuc nang");
+            int n = sc.nextInt();
+            if (n == 1) {
+                dictionaryCommandline.dictionaryManagement.repair();
+            } else if (n == 2) {
+                dictionaryCommandline.dictionaryManagement.lookup();
+            } else if (n == 3) {
+                dictionaryCommandline.Searcher();
+            } else if (n == 4) {
+                dictionaryCommandline.dictionaryManagement.showAllWord();
+            } else continue;
+            //dictionaryCommandline.dictionaryManagement.repair();
+            //dictionaryManagement.delete("lik");
+            //dictionaryCommandline.dictionaryManagement.lookup();
+        }
+
     }
 
 }
