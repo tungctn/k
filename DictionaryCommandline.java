ac8b9221cf474cd7c63a5e00684ab217f7d0b4c0
lenghia031120 <56626129+lenghia031120@users.noreply.github.com>
2021-10-19 22:18:37 +0700

    Update DictionaryCommandline.java

--- DictionaryCommandline.java
+++ DictionaryCommandline.java
@@ -10,15 +10,15 @@ public class DictionaryCommandline {
     public void Searcher() {
         Scanner sc = new Scanner(System.in);
         boolean ok = false;
-        System.out.println("nhap tu bn muon tra: ");
+        System.out.println("nhập từ bạn muốn tra: ");
         String w = sc.nextLine();
-        System.out.println("cac tu tim dc:");
+        System.out.println("các từ tìm được:");
         for (int i = 0; i < dictionaryManagement.dic.size(); i++) {
             if (dictionaryManagement.dic.getWord(i).word_target.contains(w)) {
                 System.out.println(dictionaryManagement.dic.getWord(i).word_target+": "+dictionaryManagement.dic.getWord(i).word_explain);
                 ok = true;
             }
         }
-        if(!ok) System.out.println("Khong tim thay");
+        if(!ok) System.out.println("Không tìm thấy.");
     }
 }
