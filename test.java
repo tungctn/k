bbf176b7554ae75143b7b7fb2eae2e48554245bb
lenghia031120 <56626129+lenghia031120@users.noreply.github.com>
2021-10-19 18:42:56 +0700

    Update test.java

--- test.java
+++ test.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 
 public class test {
 
-    // test các chức năng
     public static void main(String[] args) throws IOException {
 
         //Dictionary dic = new Dictionary();
 
         DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
-        //DictionaryManagement dictionaryManagement = new DictionaryManagement();
-        // test với số lượng từ là 6, copy các từ trong in.txt
+        dictionaryCommandline.dictionaryManagement.insertFromFile();
+
         Scanner sc = new Scanner(System.in);
         dictionaryCommandline.dictionaryManagement.insertFromCommandline();
 
         while (true) {
 
-            System.out.println("Chon chuc nang");
+            System.out.println("enter 1: edit word");
+            System.out.println("enter 2: translate");
+            System.out.println("enter 3: search word");
+            System.out.println("enter 4: show all word");
+            System.out.println("Choose ?");
             int n = sc.nextInt();
             if (n == 1) {
                 dictionaryCommandline.dictionaryManagement.repair();
